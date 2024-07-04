49a06981d5a09415559601e507c395ca340d41cd
danial <danialasaria@gmail.com>
2024-07-03 19:58:15 -0700

    Post length of 500 char is too small

--- api-lib/constants.js
+++ api-lib/constants.js
@@ -9,9 +9,9 @@ export const ValidateProps = {
     bio: { type: 'string', minLength: 0, maxLength: 10000 },
   },
   post: {
-    content: { type: 'string', minLength: 1, maxLength: 500 },
+    content: { type: 'string', minLength: 1, maxLength: 1000 },
   },
   comment: {
-    content: { type: 'string', minLength: 1, maxLength: 500 },
+    content: { type: 'string', minLength: 1, maxLength: 1000 },
   },
 };
